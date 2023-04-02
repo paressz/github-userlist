a5d2d778ce7d13cf66171459de0cf045aed0d377
paress <farez0830@gmail.com>
2023-04-02 17:19:42 +0700

    update coroutine dependency

--- app/src/main/java/com/farez/githubuser/data/local/UserDB.kt
+++ app/src/main/java/com/farez/githubuser/data/local/UserDB.kt
@@ -5,26 +5,24 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database (
-    entities = [FavoriteUser::class],
-    version = 1
-        )
+@Database(
+    entities = [FavoriteUser::class], version = 1
+)
 abstract class UserDB : RoomDatabase() {
     companion object {
-        var INSTANCE : UserDB? = null
+        var INSTANCE: UserDB? = null
 
         fun getDataBase(c: Context): UserDB? {
             if (INSTANCE == null) {
-                synchronized(UserDB::class){
+                synchronized(UserDB::class) {
                     INSTANCE = Room.databaseBuilder(
-                        c.applicationContext,
-                        UserDB::class.java,
-                        "user_database"
+                        c.applicationContext, UserDB::class.java, "user_database"
                     ).build()
                 }
             }
             return INSTANCE
         }
     }
+
     abstract fun favUserDAO(): FavoriteUserDAO
 }
