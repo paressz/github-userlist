cbd5c899b4c7caf74f66686452d63be973ad53b5
paress <farez0830@gmail.com>
2023-04-02 14:55:46 +0700

    fixed API key

--- app/src/main/java/com/farez/githubuser/api/Api.kt
+++ app/src/main/java/com/farez/githubuser/api/Api.kt
@@ -1,5 +1,6 @@
 package com.farez.githubuser.api
 
+import com.farez.githubuser.BuildConfig
 import com.farez.githubuser.data.model.DetailResponse
 import com.farez.githubuser.data.model.User
 import com.farez.githubuser.data.model.UserResponse
@@ -11,26 +12,27 @@ import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers ("Authorization: token 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")
+    @Headers ("Authorization: token " + BuildConfig.KEY)
     fun  getSearchUser (
         @Query("q") query : String
     ) : Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers ("Authorization: token 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")
+    @Headers ("Authorization: token " + BuildConfig.KEY)
     fun getUSerDetail(
         @Path("username") username : String
     ) : Call<DetailResponse>
 
     @GET("users/{username}/following")
-    @Headers ("Authorization: token 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")
+    @Headers ("Authorization: token " + BuildConfig.KEY)
     fun getFollowing (
         @Path("username") username : String
     ) : Call<ArrayList<User>>
 
     @GET("users/{username}/followers")
-    @Headers ("Authorization: token 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")
+    @Headers ("Authorization: token " + BuildConfig.KEY)
     fun getFollower (
         @Path("username") username : String
     ) :  Call<ArrayList<User>>
+
 }
